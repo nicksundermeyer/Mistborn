dd110653ce979d3d16fcdf01d7fba897a5ec1634
SUNDY-DESKTOP\nicks <nicksundermeyer@gmail.com>
2020-01-16 21:23:54 -0700

    Added basic enemy patrol/movement behavior

--- Assets/AI/PatrolBehavior.cs
+++ Assets/AI/PatrolBehavior.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class FollowBehavior : StateMachineBehaviour {
-    private Transform playerPos;
+public class PatrolBehavior : StateMachineBehaviour {
+    public float walkRadius = 10f;
+
     private NavMeshAgent agent;
+    private GameObject gameObject;
 
     override public void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        playerPos = GameObject.FindGameObjectWithTag ("Player").transform;
         agent = animator.GetComponent<NavMeshAgent> ();
-        agent.isStopped = false;
+        gameObject = animator.gameObject;
+        agent.ResetPath ();
     }
 
     override public void OnStateUpdate (Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        agent.SetDestination (playerPos.position);
-    }
-
-    override public void OnStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 
     }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
